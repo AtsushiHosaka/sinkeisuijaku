5d7f7e1a44deff3fb4bd82357da6c7b41dc8f1f0
Atsushi <hosakaatsushi@icloud.com>
2021-06-13 13:34:42 +0900

    Initial Commit

---
+++ sinkeisuijaku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sinkeisuijaku
+//
+//  Created by 保坂篤志 on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
